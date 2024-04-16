f2ce15d8fd71ade8addee198cce58be6493dda72
Adithya Viswanathan <adithya.viswanathan@harness.io>
2024-04-16 10:30:52 +0530

    sdfgd

---
+++ fileGenerator.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+directory = "/Users/adithyaviswanathan/gitx/test"
+
+for i in range(100):
+    file_path = directory + str(i) + ".yaml"
+    print (file_path)
+    Path(file_path).touch()
