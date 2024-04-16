23c8dcfa285a503775107ef1aa635b7f7daf80e5
Adithya Viswanathan <adithya.viswanathan@harness.io>
2024-04-16 14:38:47 +0530

    dfgsdf

--- fileGenerator.py
+++ fileGenerator.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 directory = "/Users/adithyaviswanathan/gitx/test"
 
-for i in range(300):
+for i in range(400):
     file_path = directory + str(i) + ".yaml"
     print (file_path)
     Path(file_path).touch()
